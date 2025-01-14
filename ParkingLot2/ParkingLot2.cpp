c1edb88734f380031257950252a9069fd72f54e2
krasimir_p <kpetrov@outlook.com>
2025-01-14 19:36:58 +0200

    -

---
+++ ParkingLot2/ParkingLot2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string direction;
+    string regNumber;
+    set<string> sRegNumbers;
+    while (cin >> direction && direction != "END") {
+        cin >> regNumber;
+        if (direction == "IN,") {
+            sRegNumbers.insert(regNumber);
+        }
+        else {
+            sRegNumbers.erase(regNumber);
+        }
+    }
+    if (sRegNumbers.size()) {
+        for (auto itr = sRegNumbers.begin(); itr != sRegNumbers.end(); itr++) {
+            cout << *itr << endl;
+        }
+    }
+    else {
+        cout << "Parking Lot is Empty" << endl;
+    }
+
+    return 0;
+}
